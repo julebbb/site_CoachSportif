54658bf6c0e56eb40f4d1e67deb1f4189c94fc2d
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2018-07-24 15:14:08 +0200

    footer done and contact position

---
+++ js/main.js
@@ -0,0 +1,12 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianVsZWJiYiIsImEiOiJjamp6aXJ2dHIyb3dpM3FtaDIwMnZiZjA5In0.Eop810b-IIfs27YQyrn1Tw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [3.12, 50.59], // starting position
+    zoom: 15 // starting zoom
+});
+
+
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
